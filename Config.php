fc33cac06b88bea1a413c34476ff60bfdff51049
tprt <tprtiutmdm@gmail.com>
2015-03-28 23:36:51 +0100

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,7 @@
+
+<?php
+$client_id = "5517228c485a8894775f221c";
+$client_secret = "Pz4PJr6pFJorbFRVcCHESEpaQd28v6ASmcYfJL7IMNH";
+$test_username = "your_username";
+$test_password = "your_pwd";
+?>
